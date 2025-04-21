e715fa6e6238e9fba491b13e6873efbe0d364bd8
Adam A. Jammary <adam@jammary.com>
2025-04-21 15:01:01 +0200

    improve(db): remove run-time migration because of mssql/mysql conflicts

--- SimpleWebAppMVC/Program.cs
+++ SimpleWebAppMVC/Program.cs
@@ -1,39 +1,13 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using SimpleWebAppMVC.Data;
-using System;
 
 namespace SimpleWebAppMVC
 {
     public class Program
     {
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
-
         public static void Main(string[] args)
         {
-            var host = BuildWebHost(args);
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                // Create the database (if it does not already exist)
-                try {
-                    var context = services.GetRequiredService<AppDbContext>();
-
-                    context.Database.Migrate();
-                } catch (InvalidOperationException e) {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-
-                    logger.LogError(e, "An error occurred creating the DB.");
-                }
-            }
-
-            host.Run();
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build().Run();
         }
     }
 }
